322939593338fdaaf49df17a6c9cf87f3b38d8e5
The Public Clop Accountant <PCAF@maresmating.club>
2017-11-21 00:42:17 -0500

    Add timeout to testing interface
    Also remove the test results from the repository:
    Only the default results should be committed

--- testSystem/createTestResults.py
+++ testSystem/createTestResults.py
@@ -9,17 +9,17 @@ copyfile('testconfig.cfg','../config.cfg')
 similar=0
 for fn in os.listdir('testLogs'):
 	print(fn)
-	p = subprocess.Popen(["python", "generateComic.py", '-f',"testSystem/testLogs/"+fn], cwd='..',stdout=DEVNULL, stderr=STDOUT)
-	p.wait()
-	copyfile('../comic.jpg','testResults/'+fn+'.jpg')
-	difference=compareImages.compareImages('testResults/'+fn+'.jpg','defaultResults/'+fn+'.jpg')
-	print('image is '+str(difference)+'% different than the default')
+	p = subprocess.Popen(["python3", "generateComic.py", '-f',"testSystem/testLogs/"+fn], cwd='..', stdout=DEVNULL, stderr=STDOUT)
+	p.wait(69)
+	copyfile('../comic.jpg', 'testResults/'+fn+'.jpg')
+	difference = compareImages.compareImages('testResults/'+fn+'.jpg', 'defaultResults/'+fn+'.jpg')
+	print('image is ' + str(difference) + '% different than the default')
 	if difference<3:
 		print('TEST PASS')
 		similar+=1
 	else:
 		print('TEST FAIL')
-		
+
 print(str(similar)+'/'+str(len(os.listdir('testLogs')))+' tests successful')
 
 os.remove('../config.cfg')
